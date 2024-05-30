20f300285ba2980448842c0461e18adcdf119f55
Patrick Burns <burnsoft@gmail.com>
2024-05-30 15:19:54 -0500

    wired up recent contacts

---
+++ src/server/routers/qso.tsx
@@ -0,0 +1,34 @@
+import { TRPCError } from '@trpc/server';
+import { z } from 'zod';
+
+import { createTRPCRouter, protectedProcedure } from '@/server/config/trpc';
+
+export const qsoRouter = createTRPCRouter({
+  getRecent: protectedProcedure({ authorizations: ['APP', 'ADMIN'] })
+    .input(z.void())
+    .meta({
+      openapi: {
+        method: 'GET',
+        path: '/recentContacts',
+        protect: true,
+        tags: ['recentContacts'],
+      },
+    })
+    .query(async ({ ctx }) => {
+      ctx.logger.info('Getting recent qsos');
+
+      const response = await fetch(
+        `${process.env.CLOUDLOG_API_URL}/recent_qsos/${process.env.CLOUDLOG_API_KEY}`
+      );
+
+      const qsos = await response.json();
+      if (!qsos) {
+        ctx.logger.warn('Unable to find qsos with the provided input');
+        throw new TRPCError({
+          code: 'NOT_FOUND',
+        });
+      }
+
+      return qsos;
+    }),
+});
