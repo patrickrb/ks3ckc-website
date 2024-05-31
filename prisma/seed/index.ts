6c844494f81d3d7c4d1f50c93cda7ad72cfeb394
Patrick Burns <burnsoft@gmail.com>
2024-05-31 12:35:40 -0500

    added blog and news tables and models

--- prisma/seed/index.ts
+++ prisma/seed/index.ts
@@ -2,9 +2,14 @@ import { createRepositories } from 'prisma/seed/models/repository';
 import { createUsers } from 'prisma/seed/models/user';
 import { prisma } from 'prisma/seed/utils';
 
+import { createBlogEntries } from './models/blogentry';
+import { createNewsEntries } from './models/news';
+
 async function main() {
   await createRepositories();
   await createUsers();
+  await createBlogEntries();
+  await createNewsEntries();
 }
 
 main()
